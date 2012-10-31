4b3f4aa21b583218fc6a2cb78dd396affe3c4ab4
ninj0x <ninj0x@gmail.com>
2012-10-31 00:49:34 -0700

    more

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,8 +11,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
 alias gd='git diff'
-alias gc='git commit'
-alias gca='git commit -am'
+alias gc='git commit -am'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
