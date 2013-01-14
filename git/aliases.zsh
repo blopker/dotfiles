71a7e2d116b916f7586a69620211bba51c42e97a
ninj0x <ninj0x@gmail.com>
2013-01-14 02:16:08 -0800

    git log

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,7 +8,7 @@ fi
 
 # The rest of my fun git aliases
 alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gp='git push'
 alias gd='git diff'
 alias ga="git add -A"
