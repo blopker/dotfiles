6b56130c2e52b0c5a82936ba12e47fa119d6a21f
Bo Lopker <lopkerk@gmail.com>
2013-04-26 00:38:08 -0700

    Update init.sh

--- script/init.sh
+++ script/init.sh
@@ -1,11 +1,12 @@
 APT="sudo apt-get"
-sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo add-apt-repository ppa:gophers/go
-sudo add-apt-repository ppa:a-v-shkop/chromium
-$APT update
-$APT upgrade
-$APT install vim git build-essential sublime-text golang-stable guake keepass2 chromium-browser zsh curl wget synapse
+sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
+sudo add-apt-repository -y ppa:keepassx/daily
+$APT update -y
+$APT upgrade -y
+$APT install -y vim git build-essential sublime-text guake keepassx chromium-browser zsh curl wget default-jdk
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 chsh -s /bin/zsh
+
+git clone https://github.com/blopker/dotfiles.git ~/.dotfiles
