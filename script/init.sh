de30c4842a2f10f7b210dfb595b91b6c5c1bea72
blopker <lopkerk@gmail.com>
2013-05-05 18:30:57 -0700

    added a startup script for new graphical sessions

--- script/init.sh
+++ script/init.sh
@@ -1,12 +1,31 @@
+#!/usr/bin/env bash 
+ 
+function pause(){ 
+   read -p "$*" 
+} 
+
 APT="sudo apt-get"
+
+sudo add-apt-repository -y ppa:keepassx/daily 
+$APT update 
+$APT install -y nautilus-dropbox keepassx git 
+ 
+echo "Now pull your SSH key into .ssh" 
+pause 'Press [Enter] key to continue...' 
+ 
+chmod 600 ~/.ssh/id_rsa 
+
+echo "Installing extra fun stuff..."
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
-sudo add-apt-repository -y ppa:keepassx/daily
 $APT update -y
 $APT upgrade -y
-$APT install -y vim git build-essential sublime-text guake keepassx chromium-browser zsh curl wget default-jdk
+$APT install -y vim build-essential sublime-text guake chromium-browser zsh curl wget default-jdk
 
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 
 chsh -s /bin/zsh
 
+echo "Cloning dotfiles..."
 git clone https://github.com/blopker/dotfiles.git ~/.dotfiles
+
+echo "Done!"
---
+++ script/startup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+touch /home/ninj0x/iranlolsoundspolitical
+setxkbmap -option caps:none
+xmodmap -e "keycode 66 = F12"
+
+guake &
