37b14953e72a623a6a64909b2ec80bf6f5a324b7
blopker <lopkerk@gmail.com>
2013-05-26 00:55:00 -0700

    removed unneeded aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,8 +1,8 @@
 # overides for ls
-alias ls="ls -F --color"
-alias l="ls -lAh --color"
-alias ll="ls -l --color"
-alias la='ls -A --color'
+#alias ls="ls -F --color"
+#alias l="ls -lAh --color"
+#alias ll="ls -l --color"
+#alias la='ls -A --color'
 
 # Find out what ports are in use
 alias ports="netstat -tulpn"
