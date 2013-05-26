f7e0a1d8e09d4242f683665ddcf7e75f81a78ad3
blopker <lopkerk@gmail.com>
2013-05-26 13:16:52 -0700

    added prezto to init

--- script/init.sh
+++ script/init.sh
@@ -22,11 +22,21 @@ $APT update -y
 $APT upgrade -y
 $APT install -y vim build-essential nodejs sublime-text guake chromium-browser zsh curl wget default-jdk
 
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+echo "Set up Prezto..."
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
 chsh -s /bin/zsh
 
 echo "Cloning dotfiles..."
-git clone https://github.com/blopker/dotfiles.git ~/.dotfiles
+git clone git@github.com:blopker/dotfiles.git ~/.dotfiles
+
+echo "Running bootstrap..."
+cd ~/.dotfiles
+script/bootstrap
 
 echo "Done!"
