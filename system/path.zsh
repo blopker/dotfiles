cf70b96585062373b470ff8f51c944d18aeef3b3
blopker <lopkerk@gmail.com>
2013-08-29 13:47:44 -0700

    added scala and ruby

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,6 @@
 export PATH="$HOME/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export SCALA_HOME="/home/ninj0x/scala/bin"
+export PATH=$PATH:$SCALA_HOME
