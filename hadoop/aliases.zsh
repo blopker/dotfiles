3c611147b4a112d103701a0a8b27b761a835f53d
Bo Lopker <blopker@23andme.com>
2013-12-06 12:59:00 -0800

    added things?

--- hadoop/aliases.zsh
+++
@@ -1,8 +0,0 @@
-export HADOOP="~/hadoop/hadoop-0.20.2/"
-alias hformat="$HADOOP/bin/hadoop namenode"
-alias hstart="$HADOOP/bin/start-all.sh && $HADOOP/bin/hadoop dfsadmin -safemode leave"
-alias hstop="$HADOOP/bin/stop-all.sh"
-alias hput="$HADOOP/bin/hadoop fs -put"
-alias hls="$HADOOP/bin/hadoop fs -ls"
-alias hadoop="$HADOOP/bin/hadoop"
-
