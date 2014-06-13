d2001bdebfb6f467b7b1d9d75c536210c6e699b5
blopker <lopkerk@gmail.com>
2014-06-13 13:41:19 -0700

    show dot files

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -42,3 +42,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Show them dot files
+defaults write com.apple.Finder AppleShowAllFiles YES
