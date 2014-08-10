09aef30b3fc488c5cd6284705e938885977b7736
blopker <lopkerk@gmail.com>
2014-08-10 09:20:07 -0700

    config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,10 @@
+# http://linux.die.net/man/1/zshoptions
+
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
 setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
 setopt LOCAL_TRAPS # allow functions to have local traps
@@ -11,7 +12,6 @@ setopt HIST_VERIFY
 setopt SHARE_HISTORY # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
-setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
