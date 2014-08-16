cd295445da64ab9f21e0e78ff65ffb32761121b4
blopker <lopkerk@gmail.com>
2014-08-16 03:10:20 -0700

    check brew

--- envs/init.zsh
+++ envs/init.zsh
@@ -2,4 +2,4 @@ export NVM_DIR=~/.nvm
 
 if hash rbenv 2> /dev/null; then eval "$(rbenv init -)"; fi
 if hash pyenv 2> /dev/null; then eval "$(pyenv init -)"; fi
-source $(brew --prefix nvm)/nvm.sh
+if hash brew 2> /dev/null; then source $(brew --prefix nvm)/nvm.sh; fi
