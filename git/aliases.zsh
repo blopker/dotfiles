a9c6faefb93720a5d3c52e61a41474e91aee0eab
Bo Lopker <lopkerk@gmail.com>
2015-03-07 22:25:03 -0800

    more clean up

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -3,14 +3,16 @@ alias gp='git push origin HEAD --tags'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gd='git diff'
 alias ga="git add --all :/"
+alias gam="git commit --amend -C HEAD"
 alias gc='git commit -am'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gbrm=git-delete-local-merged
 alias gg="git grep --ignore-case --line-number --full-name --color --break --heading --context 2 --show-function"
 alias greset="git reset --hard"
 alias gfb='git checkout -b' # Create new feature branch.
+
+alias gbrm=git-delete-local-merged
 
 function gi() { curl -s https://www.gitignore.io/api/$@ ;}
