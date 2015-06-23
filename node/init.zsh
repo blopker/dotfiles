73f2181d8ea3df9c9cc2e211398827744aac82c9
Bo Lopker <blopker@23andme.com>
2015-06-23 03:36:11 -0700

    update and node

--- node/init.zsh
+++ node/init.zsh
@@ -1,4 +1,6 @@
-if (($ISOSX))
-	then
+if (($ISOSX)); then
 	source $(brew --prefix nvm)/nvm.sh
+else
+    export NVM_DIR="/home/blopker/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 fi
