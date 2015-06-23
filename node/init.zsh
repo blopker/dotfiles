76099b4efd54d072630406ccc68740a7e1c6662d
Bo Lopker <blopker@23andme.com>
2015-06-23 04:12:50 -0700

    indent

--- node/init.zsh
+++ node/init.zsh
@@ -1,6 +1,6 @@
 if (($ISOSX)); then
 	source $(brew --prefix nvm)/nvm.sh
 else
-    export NVM_DIR="/home/blopker/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+	export NVM_DIR="/home/blopker/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 fi
