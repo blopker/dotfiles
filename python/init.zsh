bc4f782f73f1c3cb5d697aab48322d3f63bae6e0
Bo Lopker <blopker@23andme.com>
2015-09-06 13:17:15 -0500

    only if folder

--- python/init.zsh
+++ python/init.zsh
@@ -2,6 +2,8 @@
 export PYTHONDONTWRITEBYTECODE=true
 
 # Pyenv setup
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-if hash pyenv 2> /dev/null; then eval "$(pyenv init -)"; fi
+if [ -d "$HOME/.pyenv" ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
