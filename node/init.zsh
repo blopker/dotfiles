3fdee7090bd81ece9e43067d570388b697e3829e
Bo Lopker <blopker@23andme.com>
2015-09-06 16:10:42 -0500

    nvm is the worst

--- node/init.zsh
+++ node/init.zsh
@@ -1,6 +1,2 @@
-if (($ISOSX)); then
-	source $(brew --prefix nvm)/nvm.sh
-else
-	export NVM_DIR="/home/blopker/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-fi
+# Install n: sudo npm install -g n
+export N_PREFIX=$HOME
