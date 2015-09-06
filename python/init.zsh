c6f8a10c2a0502c2ce466dced8e9f49df5bf1368
Bo Lopker <blopker@23andme.com>
2015-09-06 13:11:15 -0500

    export pyenv

--- python/init.zsh
+++ python/init.zsh
@@ -1,4 +1,7 @@
 # screw .pyc
 export PYTHONDONTWRITEBYTECODE=true
 
+# Pyenv setup
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if hash pyenv 2> /dev/null; then eval "$(pyenv init -)"; fi
