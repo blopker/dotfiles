fb0c8d69697c425547c3705e62f4b6554f8944bf
Bo Lopker <blopker@23andme.com>
2015-11-13 21:25:46 -0800

    A little cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,23 @@
 alias reload!='. ~/.zshrc'
-alias lic='cp $ZSH/License LICENSE'
+alias update="_update && reload!"
+alias lic="cp $ZSH/LICENSE LICENSE"
 alias as='alias | grep'
-alias c='cd $PROJECTS'
+alias c="cd $PROJECTS"
 alias rm='nocorrect rm'
-alias dot='cd $ZSH'
+alias dot="cd $ZSH"
+alias x='unarchive'
+
+# Find out what ports are in use
+alias ports="netstat -tulpn"
+
+# Make directory and change into it.
+md (){ mkdir -p "$*"; cd "$*"; }
+
+# Alias pbcopy from OSX
+if ((! $ISOSX))
+    then
+    alias pbcopy='xclip -selection clipboard'
+fi
+
+# scp, but zip it first.
+tarcp (){ TEMPNAME="`date +%s`.tar"; tar czf - $1 | ssh $2 "cat > $TEMPNAME; tar xzf $TEMPNAME; rm $TEMPNAME" }
