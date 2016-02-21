9d0e72b867a69bc2d258b074411e7734045addad
Bo Lopker <blopker@23andme.com>
2016-02-20 16:29:35 -0800

    no guake

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -6,4 +6,4 @@ sudo add-apt-repository -y ppa:keepassx/daily
 $APT update
 $APT upgrade -y
 $APT install -y nautilus-dropbox keepassx git
-$APT install -y vim build-essential guake zsh curl wget
+$APT install -y vim build-essential zsh curl wget
