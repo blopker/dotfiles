ce4de6db88ce3f5927e64dfbc45dcc8ad29d102c
Bo Lopker <blopker@23andme.com>
2016-05-06 12:14:28 -0700

    sort

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,6 +6,7 @@ alias gc='git commit -am'
 alias gco='git checkout'
 alias gd='git diff'
 alias gf='git fetch --all --prune'
+alias gfind='git ls-files'
 alias gg="git grep --ignore-case --line-number --full-name --color --break --heading --context 2 --show-function"
 alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -14,7 +15,6 @@ alias gp='git push origin HEAD --tags'
 alias greset="git reset --hard"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gs='git status -sb'
-alias gfind='git ls-files'
 
 # Create new feature branch.
 gfb() { git checkout -b feature/$1; }
