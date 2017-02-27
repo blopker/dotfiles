64202bba96fef1d602a214bdb084f4a532eedfe2
Bo Lopker <blopker@23andme.com>
2017-02-26 16:30:08 -0800

    Use venv for venv

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,6 +1,6 @@
 alias activate=". venv/bin/activate"
 alias venv="virtualenv venv"
-alias venv3="virtualenv --python=python3 venv"
+alias venv3="python3 -m venv venv"
 alias py='python'
 alias serve='python -m SimpleHTTPServer'
 alias rmpyc='find . -name \*.pyc -delete'
