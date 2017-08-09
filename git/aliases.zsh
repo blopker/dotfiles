0d1180d17752f3bb518d6fb34134925a7611adf8
Bo Lopker <blopker@23andme.com>
2017-08-09 14:15:11 -0700

    iterm and better gg

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,7 +7,7 @@ alias gco='git checkout'
 alias gd='git diff'
 alias gf='git fetch --all --prune'
 alias ggf='git ls-files | grep'
-alias gg="git grep --ignore-case --line-number --full-name --color --break --heading --context 2 --show-function"
+alias gg="git grep -I --ignore-case --line-number --full-name --color --break --heading --context 2 --show-function"
 alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gme="glog --author=\"Bo Lopker\""
