9c3b3bc87494b89f5fd6e6871968e79deb224f5d
Bo Lopker <blopker@23andme.com>
2018-08-22 11:42:22 -0700

    move to drive

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,7 +6,7 @@ alias c="cd $PROJECTS"
 alias rm='nocorrect rm'
 alias dot="cd $ZSH"
 alias x='unarchive'
-alias notes="e ~/Dropbox/notes"
+alias notes="e ~/Google\ Drive/notes/"
 
 # Find out what ports are in use
 alias ports="netstat -tulpn"
