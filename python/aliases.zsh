f570e3a6f92f596db1bc1fe9a6b04515c8375b6c
Bo Lopker <lopkerk@gmail.com>
2024-02-15 20:33:36 -0800

    Wetsign2

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,4 +1,4 @@
 alias activate=". .venv/bin/activate"
-alias venv="python -m venv .venv"
+alias venv="python -m venv .venv && activate"
 alias serve="echo 'Serving: http://localhost:4836' && python -m http.server 4836"
 alias rmpyc="find . -name \*.pyc -delete"
