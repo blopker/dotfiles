5ffe39d788315ba0f6b6a76472cfe9cb4d20acba
Bo Lopker <lopkerk@gmail.com>
2024-11-07 16:57:13 +0700

    120 fps

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,10 +5,11 @@ local config = wezterm.config_builder()
 
 -- Fullscreen on startup
 wezterm.on('gui-startup', function()
-  local tab, pane, window = mux.spawn_window({})
-  window:gui_window():toggle_fullscreen()
- end)
+    local tab, pane, window = mux.spawn_window({})
+    window:gui_window():toggle_fullscreen()
+end)
 
+config.max_fps = 120
 config.color_scheme = 'iTerm2 Pastel Dark Background'
 config.font_size = 20
 config.font = wezterm.font 'Monaco'
@@ -31,27 +32,27 @@ config.keys = {
     -- Sends ESC + b and ESC + f sequence, which is used
     -- for telling your shell to jump back/forward.
     {
-      key = 'LeftArrow',
-      mods = 'OPT',
-      action = act.SendString '\x1bb',
+        key = 'LeftArrow',
+        mods = 'OPT',
+        action = act.SendString '\x1bb',
     },
     {
-      key = 'RightArrow',
-      mods = 'OPT',
-      action = act.SendString '\x1bf',
+        key = 'RightArrow',
+        mods = 'OPT',
+        action = act.SendString '\x1bf',
     },
 
     -- Tab navigation
     { key = 'RightArrow', mods = 'SUPER', action = act.ActivateTabRelative(1) },
-    { key = 'LeftArrow', mods = 'SUPER', action = act.ActivateTabRelative(-1) },
+    { key = 'LeftArrow',  mods = 'SUPER', action = act.ActivateTabRelative(-1) },
 
     -- Open config file
     {
         key = ',',
         mods = 'SUPER',
         action = act.SpawnCommandInNewTab {
-          cwd = wezterm.home_dir,
-          args = { 'code', wezterm.config_file },
+            cwd = wezterm.home_dir,
+            args = { 'code', wezterm.config_file },
         },
     },
     {
@@ -60,17 +61,17 @@ config.keys = {
         action = act.ToggleFullScreen,
     },
 
-    { key = 'w', mods = 'SUPER', action = act.CloseCurrentPane({confirm = false}) },
-    { key = 'w', mods = 'SUPER|SHIFT', action = act.CloseCurrentTab({confirm = false}) },
+    { key = 'w',          mods = 'SUPER',       action = act.CloseCurrentPane({ confirm = false }) },
+    { key = 'w',          mods = 'SUPER|SHIFT', action = act.CloseCurrentTab({ confirm = false }) },
 
     -- Pane navigation
-    { key = 'UpArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Up', },
-    { key = 'DownArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Down', },
-    { key = 'LeftArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Left', },
+    { key = 'UpArrow',    mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Up', },
+    { key = 'DownArrow',  mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Down', },
+    { key = 'LeftArrow',  mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Left', },
     { key = 'RightArrow', mods = 'SUPER|SHIFT', action = act.ActivatePaneDirection 'Right', },
-    { key = '\\', mods = 'SUPER', action = act.SplitVertical, },
-    { key = '|', mods = 'SUPER', action = act.SplitHorizontal, },
+    { key = '\\',         mods = 'SUPER',       action = act.SplitVertical, },
+    { key = '|',          mods = 'SUPER',       action = act.SplitHorizontal, },
 
-  }
+}
 
 return config
